c5d142a4f6d8b09f8c4fab1e041aa092c6f017b7
Xayzo <deonseorang@gmail.com>
2022-02-23 16:51:22 +0700

    Update musichelp.py

--- EmikoRobot/modules/musichelp.py
+++ EmikoRobot/modules/musichelp.py
@@ -1,19 +1,17 @@
-__mod_name__ = "Musicplayer"
+__mod_name__ = "Music Player"
 
 __help__ = """
-──「 Music stream 」──              
+──「 Music Player 」──              
 
-❂ /play (query or reply audio) to playing music from yourube or your audio telegram.
-❂ /playlist (choose option) for playing your personal playlist or group playlist.
+❂ /dplay (query or reply audio) - to playing music from youtube or your audio telegram.
+❂ /playlist (choose option) - for playing your personal playlist or group playlist.
 
 • Admins only
 
-❂ /auth - Add to authorized users.
-❂ /unauth - Canceling or deleting from authorized users list.
-❂ /msettings - To open panel settings on musicplayer.
-❂ /setadmin - To set all command only for admins.
-❂ /setmember - Set all command for all members.
-❂ /vol (range 1-200) - To set volume on voice calls.
+❂ /pause - To pause the playback.
+❂ /resume - To resuming the playback You've paused.
+❂ /skip - To skipping the playback.
+❂ /end - For end the playback.
 
 NOTE : DON'T SPAMMING AT REQUEST SONG OR VIDEO...
 """
